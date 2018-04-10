20d4e04eb3ad2a5e18152cba50f958715ec3b54b
Frank Rose <frankrosework@gmail.com>
2018-04-10 00:54:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,12 +1,13 @@
 //define functions here
-
-$(document).ready(function(){
-
-// call functions here
 function getIt() {
   $('p').on('click', function() {
     alert('Hey!');
   });
 }
+
+$(document).ready(function(){
+
+// call functions here
+getIt();
 
 });
